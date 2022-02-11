905fc1447db6b67a398270c5d779214754751313
Katherine Quinn <90004249+quinnyyk@users.noreply.github.com>
2022-02-11 12:06:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 app.get('http://localhost:1337/page1', function (req, res) {
 res.sendFile('index.html', {root: './' })
 })
-app.listen(1337, () => console.log('Marist Chatter listening on port
-1337!'));
+app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
 
 
