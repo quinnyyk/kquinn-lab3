5cd8b55d5d9bf520dfd312c82a4c4274247b6b9d
Katherine Quinn <90004249+quinnyyk@users.noreply.github.com>
2022-02-11 12:09:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('http://localhost:1337/page1', function (req, res) {
-res.sendFile('index.html', {root: './' })
+res.sendFile('page1.html', {root: './' })
 })
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
 
