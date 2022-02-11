843e82ca41dc33cbb1dc250072a30f6e3b14552d
Katherine Quinn <90004249+quinnyyk@users.noreply.github.com>
2022-02-11 08:06:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+var http = require('http');
+http.createServer(function (request, response) {
+response.writeHead(200, {
+'Content-Type': 'text/plain'
+});
+response.end('Hello World\n');
+}).listen(1337);
+console.log('Server running on port 1337!');
 
