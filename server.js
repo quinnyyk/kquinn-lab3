b659f8a4b5f43759c0d79bfd53cfdd68c2b5c869
quinnyk <73131737+quinnyk@users.noreply.github.com>
2022-02-13 17:39:18 -0500

    updates

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 
+
 app.get('/', function (req, res) {
 	res.sendFile('index.html', {root: './' })
 })
-app.get('/page1', function (req, res) {
-	res.sendFile('http://localhost:1337/page1.html', {root: './' });
+app.get('/page1.html', function (req, res) {
+	res.sendFile(__dirname + 'page1.html');
 })
+
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
 
 		
