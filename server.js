8c829b671a0491600b5b9a582cff34b617b20456
quinnyk <73131737+quinnyk@users.noreply.github.com>
2022-02-13 18:05:59 -0500

    got links to work back and forth between pages

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
-
-
 app.get('/', function (req, res) {
-	res.sendFile('index.html', {root: './' })
+res.sendFile('index.html', {root: './' })
 })
-app.get('/page1.html', function (req, res) {
-	res.sendFile(__dirname + 'page1.html');
+app.get('/page1.html', function(req,res) {
+	res.sendFile('page1.html', {root: './'})
+})
+app.get('/index.html', function(req,res) {
+	res.sendFile('index.html', {root: './'})
 })
 
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
-
-		
